fc3b4dbff2b8375f3d7e6b7fed456f176202d263
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-12-02 12:54:10 +0530

    Addded Skills Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Intro from "./components/intro";
 import Sectiondivider from './components/sectiondivider';
 import About from './components/about';
 import Projects from './components/projects';
+import Skills from "./components/skills";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Intro /> 
         <Sectiondivider />
         <About />
-        <Projects />  
+        <Projects />
+        <Skills />  
     </main>
   )
 }
