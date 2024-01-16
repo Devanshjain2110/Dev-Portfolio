0c5086ac3920265a61b405f00966b6ae10e4a14c
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-16 22:02:50 +0530

    Corrected some height

--- app/components/BuyCoffee.tsx
+++ app/components/BuyCoffee.tsx
@@ -4,7 +4,7 @@ import Coffee from '../../public/coffee.png'
 
 function BuyCoffee() {
   return ( 
-  <a target='_blank' className="fixed bottom-5 left-5 bg-white w-[3rem] h-[3rem] pt-1 -rotate-12 border borderBlack dark:border-white rounded-full dark:bg-yellow-400" href="https://www.buymeacoffee.com/devanshjain" ><Image src={Coffee} alt="coffee" width={100} height={100}></Image></a>
+  <a target='_blank' className="fixed bottom-5 left-5 bg-white w-[3.1rem] h-[3.1rem] pt-1 -rotate-12 border borderBlack dark:border-white rounded-full dark:bg-yellow-400" href="https://www.buymeacoffee.com/devanshjain" ><Image src={Coffee} alt="coffee" width={100} height={100}></Image></a>
   )
 }
 
